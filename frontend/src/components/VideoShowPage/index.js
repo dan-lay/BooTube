9b57647f2801fd1a33cddb7a8863d37b36157c06
dan-lay <mr.danlay@gmail.com>
2022-10-21 04:03:21 -0400

    double checking before pushing to heroku

--- frontend/src/components/VideoShowPage/index.js
+++ frontend/src/components/VideoShowPage/index.js
@@ -1,10 +1,12 @@
 import './VideoShowPage.css';
-
 import MastHead from '../VideoIndexPage/MastHead';
 import VideoRightIndexSide from './VideoRightIndexSide';
 import VideoDisplaySide from './VideoDisplaySide';
+import { Redirect } from 'react-router-dom';
 
 const VideoShowPage = () => {
+
+   if (!currentUser) return <Redirect to="/"/>;
 
    return (
       <div className='video-show-page'>
