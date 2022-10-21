9b3bf8e3bd69c8feb46980ed383864d6f7089e15
dan-lay <mr.danlay@gmail.com>
2022-10-21 09:07:21 -0400

    get rid of duplicate files

---
+++ frontend/src/components/VideoShowPage/VideoRightIndexSide/VideoSideIndex/VideoSideIndexItem/index 2.js
@@ -0,0 +1,13 @@
+import './VideoSideIndexItem.css'
+
+const VideoSideIndexItem = () => {
+
+   return (
+      <div className='video-side-index-item'>
+
+      </div>
+   )
+
+}
+
+export default VideoSideIndexItem;
