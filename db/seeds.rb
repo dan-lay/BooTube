77913f4f479e24faa0aa25e1efa3c41a1fd20099
dan-lay <mr.danlay@gmail.com>
2022-12-15 23:19:15 -0500

    add demo description and date formatter

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,14 +37,11 @@ ActiveRecord::Base.transaction do
 
   puts "Creating videos..."
 
-  demo_vid = Video.new(title: "i am the demo video", uploader_id: demo.id)
-  puts "after vid instantiated"
+  demo_vid_description = "This video is from when our DIY home-improvement project went horribly wrong. As you can see I was very visibly frustrated, as was my husband in the background..."
+  demo_vid = Video.new(title: "i am the demo video", uploader_id: demo.id, description: demo_vid_description)
   demo_media_object = URI.open('https://booootube-dev.s3.amazonaws.com/zombie_chilling_out.mp4')
-  puts "after uri opened"
   demo_vid.media_object.attach(io: demo_media_object, filename: "zombie_chilling.mp4")
-  puts "after attachment"
   demo_vid.save!
-  puts "after save"
 
   dan_vid = Video.new(title: "dan's cool vid", uploader_id: dan.id)
   dan_media_object = URI.open('https://booootube-dev.s3.amazonaws.com/corn_lady.mp4')
--- frontend/src/components/VideoShowPage/VideoDisplaySide/VidInfoBox/VidInfoBox.js
+++ frontend/src/components/VideoShowPage/VideoDisplaySide/VidInfoBox/VidInfoBox.js
@@ -4,6 +4,36 @@ import { useEffect } from 'react';
 
 const VidInfoBox = () => {
    const video = useSelector(state => state.videos ? state.videos[1] : null)
+   const uploadTime = video ? video.createdAt : null;
+   const description = video ? video.description : null;
+   
+   const formatUploadDate = date => {
+      const then = new Date(date);
+      const now = new Date();
+
+      let timeText;
+      let timeDiff;
+      let timeElapsed = (now - then) / 1000;
+
+      if (Math.round(timeElapsed / 31540000) >= 1) {
+         timeDiff = Math.round(timeElapsed /= 31540000);
+         timeText = timeDiff === 1 ? "year" : "years";
+      } else if (Math.round(timeElapsed / 2628000) >= 1) {
+         timeDiff = Math.round(timeElapsed /= 2628000);
+         timeText = timeDiff === 1 ? "month" : "months";
+      } else if (Math.round(timeElapsed / 86400) >= 1) {
+         timeDiff = Math.round(timeElapsed /= 86400);
+         timeText = timeDiff === 1 ? "day" : "days"
+      } else if (Math.round(timeElapsed / 3600) >= 1) {
+         timeDiff = Math.round(timeElapsed /= 3600);
+         timeText = timeDiff === 1 ? "hour" : "hours"
+      } else {
+         timeDiff = Math.round(timeElapsed /= 60);
+         timeText = timeDiff === 1 ? "minute" : "minutes"
+      }
+
+      return `${timeDiff} ${timeText} ago`
+   }
 
    return (
       <div className='vid-info-box'>
@@ -31,7 +61,11 @@ const VidInfoBox = () => {
          </div>
          <div className='vid-description-container'>
             <div className='vid-description'>
-
+               <div className='view-timestamp-container'>
+                  <p className='viewcount'>views</p>
+                  <p className='timestamp'>{`${uploadTime ? formatUploadDate(uploadTime) : null}`}</p>
+               </div>
+               <p className='description'>{description}</p>
             </div>
          </div>
 
