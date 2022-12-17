9007e9174f9a4f199c56a7ec6809dc3b78fc3da8
dan-lay <mr.danlay@gmail.com>
2022-12-16 19:33:37 -0500

    begin style of index as well

--- app/views/api/videos/index.json.jbuilder
+++ app/views/api/videos/index.json.jbuilder
@@ -1,7 +1,8 @@
 @videos.each do |video|
    json.set! video.id do
-      json.extract! video, :id, :title, :description
+      json.extract! video, :id, :title, :description, :uploader_id, :created_at, :updated_at
       json.mediaObject video.media_object.url
+      json.uploaderName video.uploader.first_name #change to username later
    end
 end
 
--- frontend/src/components/VideoIndexPage/VideoIndex/VideoIndexItem/VideoIndexItem.js
+++ frontend/src/components/VideoIndexPage/VideoIndex/VideoIndexItem/VideoIndexItem.js
@@ -1,19 +1,42 @@
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import './VideoIndexItem.css';
+import { formatUploadDate } from '../../../../utils/dateFormatter.js';
 
 const VideoIndexItem = (props) => {
    const video = props.video;
    const history = useHistory();
+   const uploaderName = video ? video.uploaderName : null;
+   const uploadDate = video ? formatUploadDate(video.createdAt) : null;
+
 
    return (
       <Link to={`/videos/${video.id}`}>      
-         <div className="video-index-item" >
-            <video className='thumbnail'>
-               <source type="video/mp4" src={video.mediaObject}/>
-            </video>
+         <div className="video-index-item">
+            <div className='thumbnail-container'>
+               <video className='thumbnail'>
+                  <source type="video/mp4" src={video.mediaObject}/>
+               </video>
+            </div>
             <div className='index-item-bottom'>
-               <p className='index-title'>{video.title}</p>
+               <div className='index-item-left'>
+
+               </div>
+               <div className='index-item-right'>
+                  <div className='item-title-container'>
+                     <p className='index-title'>{video.title}</p>
+                  </div>
+                  <div className='item-info-container'>
+                     <p className='item-channel-name'>{uploaderName}</p>
+                     <div className='item-info'>
+                        <p className='item-uploader'>views</p>
+                        <p className='spacer'>.</p>
+                        <p className='item-date'>{uploadDate}</p>
+                     </div>
+                     
+                  </div>
+
+               </div>
             </div>
          
          </div>
